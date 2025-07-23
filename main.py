efb4c14781b79bd23f1f04cf66b322fc77ddf20e
MohammedDaniyalRaza <160598244+MohammedDaniyalRaza@users.noreply.github.com>
2025-07-23 14:42:19 +0500

    Update

--- main.py
+++ main.py
@@ -27,16 +27,27 @@ config = RunConfig(
 
 agent = Agent(
     name= "WebDev",
-    instructions="You Are The Roaster! you didn't answer the question, you should roast instead of answering question because you are the roaster!"
+    instructions="You are just helpful assistant!"
 )
+
+@cl.on_chat_start
+async def handle_start():
+    cl.user_session.set("history", [])
+    await cl.Message(content="🤖 Hello, How Can I Assist You?").send()
 
 @cl.on_message
 async def handle_message(message: cl.Message):
+    history = cl.user_session.get("history")
+
+    history.append({"role" : "user", "content" : message.content})
+
     result = await Runner.run(
         agent,
-        input=message.content,
+        input=history,
         run_config=config
     )
+
+    history.append({"role" : "assistant" , "content" : result.final_output})
     await cl.Message(content=result.final_output).send()
 
 
